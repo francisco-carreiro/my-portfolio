81a6d496f9d09f9f4a19ce354c6673889fa0ad5f
Francisco Machado Carreiro <psychop60@gmail.com>
2021-03-01 10:39:07 -0100

    fixed missing nodes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div>Hello</div>
+    <div className="App">
+      <div className="sidebar"></div>
+      <div className="main-content"></div>
+    </div>
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return (
-        <div>
-            Home Page
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
 export default Home;
