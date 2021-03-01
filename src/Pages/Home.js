d8a215d1a627e471493786886c6e0d3d5a2cbe1d
Francisco Machado Carreiro <psychop60@gmail.com>
2021-03-01 14:09:27 -0100

    Icons placed base structure done now passing to other pages

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -6,13 +12,22 @@ function Home() {
       <header className="hero">
         <h1 className="hero-text">
           Hi, I am
-          <span>Francisco.</span>
+          <span> Francisco.</span>
         </h1>
-        <p className="h-sub-text">
-          Info about myself.............
-        </p>
+        <p className="h-sub-text">Info about myself.............</p>
         <div className="icons">
-
+          <Link className="icon-holder">
+            <FontAwesomeIcon icon={faFacebook} className="icon fb" />
+          </Link>
+          <Link className="icon-holder">
+            <FontAwesomeIcon icon={faInstagram} className="icon in" />
+          </Link>
+          <Link className="icon-holder">
+            <FontAwesomeIcon icon={faGithub} className="icon gh" />
+          </Link>
+          <Link className="icon-holder">
+            <FontAwesomeIcon icon={faLinkedin} className="icon in" />
+          </Link>
         </div>
       </header>
     </div>
