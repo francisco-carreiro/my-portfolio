9e5078c4ea4f369a9f1ea076cc47fdb5709f3d6e
Francisco Machado Carreiro <psychop60@gmail.com>
2021-03-01 11:58:06 -0100

    Starting to look like something good!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.scss";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <NavBar />
+      </div>
       <div className="main-content"></div>
     </div>
   );
