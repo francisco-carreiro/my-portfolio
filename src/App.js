2b7b4097777ff69b8fb12df2cf7e63521bfbf8fa
Francisco Machado Carreiro <psychop60@gmail.com>
2021-03-11 11:49:35 -0100

    Main page queries done now working on about page queries

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import About from "./Pages/About";
 import Projects from "./Pages/PortfoliosPage";
 import Contact from "./Pages/Contacts";
 import { Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () => {
+    setNavToggle(!navToggle);
+  };
+
   return (
     <div className="App">
-      <div className="sidebar">
+      <div className={`sidebar ${navToggle ? "nav-toggle" : ""}`}>
         <NavBar />
+      </div>
+      <div className="nav-btn" onClick={navClick}>
+        <div className="line-1"></div>
+        <div className="line-2"></div>
+        <div className="line-3"></div>
       </div>
       <div className="main-content">
         <div className="content">
