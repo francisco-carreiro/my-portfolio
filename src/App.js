f26d00d1a81c4c9dcbb0ef1a42b62ebc0ee55b1f
Francisco Machado Carreiro <psychop60@gmail.com>
2021-03-22 14:03:11 -0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,10 +27,8 @@ function App() {
       <div className="main-content">
         <div className="content">
           <Switch>
-          <Route  path="my-portfolio/" exact>
-              <Home />
-            </Route>
-            <Route  path="/" exact>
+            <Route path="my-portfolio/" exact></Route>
+            <Route path="/" exact>
               <Home />
             </Route>
             <Route path="/about" exact>
